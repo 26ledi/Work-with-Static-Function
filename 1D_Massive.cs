ff1b02c75ce23bb413908f6ed0ce65aa58911659
26ledi <joyceledi26@gmail.com>
2023-02-06 18:15:11 +0300

    first commit

---
+++ 1D_Massive.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Csharp_Lab3
+{
+    public class _1D_Massive
+    {
+        //field
+        private int[] array;
+        public int n { get; set; }
+        public int[] GetElementFromArray()//for using array
+        {
+            return array;
+        }
+        public void SetElementFromArray(int[] array)
+        {
+            this.array = array;
+        }
+
+        public int this[int index]
+        {
+            get
+            {
+                return array[index];
+            }
+            set
+            {
+                array[index] = value;
+            }
+        }
+       
+        public _1D_Massive(int n)//constructor
+        {
+            array = new int[n];
+            this.n = n;
+        }
+
+       
+        public _1D_Massive(int[] array)
+        {
+            this.array = array;
+            this.n = array.Length;
+        }
+
+        public void WriteArr(string name) //methode to fill array 
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine($"Array {name}[{i + 1}]=");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+
+        }
+        public void ShowArr() //methode to fill array
+        {
+            for (int i = 0; i < array.Length; i++)
+                Console.WriteLine($"{array[i]}\t");
+        }
+        public static double Sum(params int[][] array) 
+        {
+            int Sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array[i].Length; j++)
+                {
+                    if (array[i][j] < 0)
+                    {
+                        Sum = Sum + array[i][j];
+                    }
+                }
+
+            }
+            return Sum;
+        }
+        public static _1D_Massive operator !(_1D_Massive Tab)
+        {
+            _1D_Massive arrayNew = new _1D_Massive(Tab.GetElementFromArray().Length);
+            int c = 0;
+            for (int i = 0; i < Tab.GetElementFromArray().Length; i++)
+            {
+                if (Tab[i] > 0)
+                {
+                    arrayNew[c] = Tab[i] * (-1);
+                }
+                else
+                {
+                    arrayNew[c] = Math.Abs(Tab[i]);
+                }
+                c++;
+            }
+            return arrayNew;
+        }
+
+        public static _1D_Massive operator *(_1D_Massive tab1, int n)
+        {
+            for (int i = 0; i < tab1.array.Length; i++)
+            {
+                tab1[i] *= n;
+            }
+            return tab1;
+        }
+      
+        public static _1D_Massive operator *(int n, _1D_Massive tab1)
+        {
+            for (int i = 0; i < tab1.array.Length; i++)
+            {
+                tab1[i] *= n;
+            }
+            return tab1;
+        }
+        public static float SumNeg2(int[] array)//Sum neg of Array A or B or C
+        {
+            int sum = 0;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] < 0)
+                {
+                    sum = sum + array[i];
+                }
+            }
+
+            return sum;
+        }
+        
+      
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Text;
+
+namespace Csharp_Lab3
+{
+    internal class Program 
+    {
+        static void Main(string[] args) 
+
+        {
+            
+            Console.WriteLine("Enter the size of array A:");
+            int n1=Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the size of array B:");
+            int n2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the size of array C:");
+            int n3 = Convert.ToInt32(Console.ReadLine());
+            _1D_Massive massiveA = new _1D_Massive(n1);
+            _1D_Massive massiveB = new _1D_Massive(n2);
+            _1D_Massive massiveC = new _1D_Massive(n3);
+            while (true) 
+            {
+                Console.WriteLine("Input the Array A:");
+                massiveA.WriteArr("A");
+                Console.WriteLine("Output the Array A:");
+                massiveA.ShowArr();
+                Console.WriteLine("Input the Array B:");
+                massiveB.WriteArr("B");
+                Console.WriteLine("Output the Array B:");
+                massiveB.ShowArr();
+                Console.WriteLine("Input the Array C:");
+                massiveC.WriteArr("C");
+                Console.WriteLine("Output the Array C:");
+                massiveC.ShowArr();
+                Console.WriteLine("The sum of negative array's A elements");
+                Console.WriteLine(_1D_Massive.Sum(massiveA.GetElementFromArray()));
+                Console.WriteLine("The sum of negative array's B elements");
+                Console.WriteLine(_1D_Massive.Sum(massiveB.GetElementFromArray()));
+                Console.WriteLine("The sum of negative array's C elements");
+                Console.WriteLine(_1D_Massive.Sum(massiveC.GetElementFromArray()));
+                Console.WriteLine("Transform element of Array A");
+                _1D_Massive multA = !(massiveA);
+                multA.ShowArr();
+                Console.WriteLine("Transform element of Array B");
+                _1D_Massive multB = !(massiveB);
+                multB.ShowArr();
+                Console.WriteLine("Transform element of Array C");
+                _1D_Massive multC = !(massiveC);
+                multC.ShowArr();
+                Console.WriteLine("A*5:");
+                Console.WriteLine("------------");
+                _1D_Massive multA_with_5 = (massiveA * 5);
+                multA_with_5.ShowArr();
+                Console.WriteLine("C*5:");
+                Console.WriteLine("------------");
+                _1D_Massive multC_with_5 = (massiveC * 5);
+                multC_with_5.ShowArr();
+                Console.Write("2*B:");
+                Console.WriteLine("------------");
+                _1D_Massive multB_with_2 = (2 * massiveC);
+                multB_with_2.ShowArr();
+                Console.Write("2*(-A):");
+                Console.WriteLine("------------");
+                _1D_Massive mult2_with_minusA = (massiveA*(-1) * 2);
+                mult2_with_minusA.ShowArr();
+                Console.WriteLine("C*4:");
+                Console.WriteLine("------------");
+                _1D_Massive multC_with_4 = (massiveC * 4);
+                multB_with_2.ShowArr();
+                double Sum1 = _1D_Massive.Sum(massiveA.GetElementFromArray());
+                double Sum2 = _1D_Massive.Sum(massiveB.GetElementFromArray());
+                if (Sum1 > Sum2)//if Sum of negative element's A are > B
+                {
+
+                    for(int i = 0; i < massiveA.n; i++) 
+                    {
+                       for (int j = 1; j < massiveA.n; j++) 
+                        {
+                            if (massiveA[i]<0 && massiveA[i]== massiveA[j]) 
+                            {
+                                massiveA[i] = Convert.ToInt32(Sum1);
+                               
+                            }
+                        }
+                    }
+                    Console.WriteLine("Last Array");
+                    massiveA.ShowArr();
+                }
+                
+                else
+                {
+                    Console.WriteLine("The Sum1 is less than the Sum2");
+                }
+                break;
+            }
+            Console.ReadKey();
+            Console.Clear();
+
+        }
+    }
+}
